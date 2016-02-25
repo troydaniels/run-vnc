58f33ddfe2a65d7c73d8235549435658a225c017
Troy Daniels <troy_daniels@live.com.au>
2016-02-26 01:15:48 +1100

    Heavy refactor

--- runVNC.sh
+++ runVNC.sh
@@ -1,20 +1,16 @@
+
 #!/bin/bash
 
 # runVNC.sh
 # Small script to allow the launching of VNC Viewer from a desktop icon
 # on OSX/Linux
-# Tested on OSX - Install paths may have to be modified on other machines
+#
 # Troy Daniels 25/2/16
 
-host=104.199.146.199::5902
-unamestr=$(uname)
+host=104.199.146.199::5909
 
-#Paths
-macPath="/Applications/RealVNC/VNC Viewer.app/Contents/MacOS"
-linuxPath="/usr/bin"  # Depending on install, may be /usr/local/bin
+noInstall="ERROR: VNC Viewer not found. Please download and try again."
 
-if [[ "$unamestr" =~ Darwin ]]; then # Running OSX
-  "$macPath/vncviewer" $host
-elif [[ "$unamest" =~ Linux ]]; then # Maybe some awesome person is running Debian??
-  ./"$linuxPath/vncviewer" $host
-fi
+path=$(which vncviewer) || (>&2 echo "$noInstall" && exit 1)
+
+"$path" "$host"
