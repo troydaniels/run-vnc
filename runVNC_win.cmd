b08e6a22a869df32b6d235fa0e0789d52395a685
Troy Daniels <troy_daniels@live.com.au>
2016-02-29 23:47:29 +1100

    Batch file syntax fix

--- runVNC_win.cmd
+++ runVNC_win.cmd
@@ -1,19 +1,19 @@
 :: runVNC_win.cmd
 :: Small batch script to allow the launching of VNC Viewer from Windows
-:: to the correct host (untested)
+:: to the correct host
+::
 :: Troy Daniels 25/2/16
+
+
 @ECHO OFF
 
 SET host="104.199.146.199::5902"
-
 SET noInstall="ERROR: VNC Viewer not found. Please download and try again."
-
-SET path=where vncviewer
+SET path=C:\Program Files\RealVNC\VNC Viewer\vncviewer.exe
+SET passPath=C:\Program Files\RealVNC\VNC Viewer\passwd
 
 IF NOT EXIST %path%(
-    ECHO %noInstall%
-    EXIT /B 1
-    )
+    ECHO %noInstall%    
+)
 
-START "" "%path%" "%host%" 
-
+START "" "%path%" -passwd "%passPath%" "%host%" 
